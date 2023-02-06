6a4f70ba916ea4f45e4efea5d22d75dbc39838ef
Resul <bm.resulaydin@gmail.com>
2023-02-06 04:02:00 +0300

    9. Derse başlandı ve Core Yapısı inşa edilmeye başlandı. 06-02-2023

--- Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
+++ Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
@@ -1,12 +1,67 @@
-﻿using System;
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Core.DataAccess.EntityFramework
 {
-    internal class EfEntityRepositoryBase
+    // Burada ister T ister TEntity yazarız çok önemli değil .
+    public class EfEntityRepositoryBase<TEntity,TContext> :IEntityRepository<TEntity>
+        where TEntity: class,IEntity,new()
+        where TContext: DbContext,new()// Neden burada 'class' kullanılmadı
     {
+        public void Add(TEntity entity)
+        {
+            // Bu 
+            /* Eski Yöntem*//**
+            using (TContext context = new TContext())
+            {
+                var addedEntity = context.Entry(entity);
+                addedEntity.State = EntityState.Added;
+                context.SaveChanges();
+            }
+            */
+            using TContext context = new TContext();
+            var addedEntity = context.Entry(entity);
+            addedEntity.State = EntityState.Added;
+            context.SaveChanges();
+        }
+
+        public void Delete(TEntity entity)
+        {
+            using TContext context = new TContext();
+            var deletedEntity = context.Entry(entity);
+            deletedEntity.State = EntityState.Deleted;
+            context.SaveChanges();
+
+        }
+
+        public TEntity Get(Expression<Func<TEntity, bool>> filter)
+        {
+            using TContext context = new TContext();
+            return context.Set<TEntity>().SingleOrDefault(filter) ?? new TEntity();
+        }
+
+        public List<TEntity> GetAll(Expression<Func<TEntity, bool>> filter = null)
+        {
+            using TContext context = new TContext();
+            return filter == null
+                ? context.Set<TEntity>().ToList()
+                : context.Set<TEntity>().Where(filter).ToList();
+        }
+
+        public void Update(TEntity entity)
+        {
+            using (TContext context = new TContext())
+            {
+                var updatedEntity = context.Entry(entity);
+                updatedEntity.State = EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
     }
 }
--- DataAccess/Concrete/EntityFramework/EfProductDal.cs
+++ DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -1,4 +1,5 @@
-﻿using DataAccess.Abstract;
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
 using Entities.Concrete;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -10,56 +11,17 @@ using System.Threading.Tasks;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-    public class EfProductDal : IProductDal
+    /* Normalde bu - 
+        (X)  public class EfProductDal : EfEntityRepositoryBase<Product, NorthwindContext>
+      
+       1- Bu yapı(X) işimizi görür diye düşünüyorken Engin hoca imdada yetişti ve 'IProductDal'
+       yapısının ise Product ile ayrıca işlemler olacaksa yani ileride sadece Product ile ilgili DB Joinleme
+       işlermleri Dto lar tarafından olacaksa sadece X yapısına bağımlı bir durum olmasın diye
+       2- Ayrıca bizim SOLID ' de bulunan 'O' kuralına uygun olamsı için yani Business tarafı 'IProductDal' kullanmaktadır
+          Dolayısıyla yapının bozulmaması için 'IProductDal' implementasyonunu da yapacaktır. (ÇOOK ÖNEMLİ)
+     */
+    public class EfProductDal : EfEntityRepositoryBase<Product,NorthwindContext>,IProductDal
     {
-        public void Add(Product entity)
-        {
-            // Bu 
-            /* Eski Yöntem*//**
-            using (NorthwindContext context = new NorthwindContext())
-            {
-                var addedEntity = context.Entry(entity);
-                addedEntity.State = EntityState.Added;
-                context.SaveChanges();
-            }
-            */
-            using NorthwindContext context = new NorthwindContext();
-            var addedEntity = context.Entry(entity);
-            addedEntity.State = EntityState.Added;
-            context.SaveChanges();
-        }
 
-            public void Delete(Product entity)
-            {
-                using NorthwindContext context = new NorthwindContext();
-                var deletedEntity = context.Entry(entity);
-                deletedEntity.State = EntityState.Deleted;
-                context.SaveChanges();
-
-            }
-
-            public Product Get(Expression<Func<Product, bool>> filter)
-            {
-            using NorthwindContext context = new NorthwindContext();
-            return context.Set<Product>().SingleOrDefault(filter) ?? new Product();
-        }
-
-            public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
-            {
-            using NorthwindContext context = new NorthwindContext();
-            return filter == null
-                ? context.Set<Product>().ToList()
-                : context.Set<Product>().Where(filter).ToList();
-        }
-
-            public void Update(Product entity)
-            {
-                using (NorthwindContext context = new NorthwindContext())
-                {
-                    var updatedEntity = context.Entry(entity);
-                    updatedEntity.State = EntityState.Modified;
-                    context.SaveChanges();
-                }
-            }
-        }
     }
+}
